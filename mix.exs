eb7be298fd98b5478e553b428d8ad4c3ae75f0dd
Keith Brings <keith.brings@noizu.com>
2023-08-03 08:31:30 +0700

    hex package details

--- mix.exs
+++ mix.exs
@@ -58,6 +58,7 @@ defmodule Noizu.Services.MixProject do
     [
       {:syn, "~> 3.3"},
       {:junit_formatter, "~> 3.3", only: [:test]},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]|> then(fn(deps) ->
